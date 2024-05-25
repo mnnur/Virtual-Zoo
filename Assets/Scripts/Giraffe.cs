9845d1222e7dad446a2d9e920071e77ef3b07073
mmnur <nurramadan35@gmail.com>
2024-05-25 14:39:37 +0700

    Add AI walk

--- Assets/Scripts/Giraffe.cs
+++ Assets/Scripts/Giraffe.cs
@@ -1,15 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.InputSystem.Android.LowLevel;
+using UnityEngine.AI;
+
 
 public class Giraffe : MonoBehaviour
 {
     Animation anim;
     AnimalState animalState = AnimalState.IDLE;
+
+    GameObject player;
+    NavMeshAgent agent;
+    [SerializeField] LayerMask groundLayer, playerLayer;
+    Vector3 destinationPoint;
+    bool walkPointSet;
+    [SerializeField] float range;
+
     Transform lookAt;
     Transform moveTo;
-    public int hunger = 80;
+    public int hunger = 40;
     int moveSpeed = 10;
     int runSpeed = 20;
     float randomWalkRadius = 5f;
@@ -22,7 +30,7 @@ public class Giraffe : MonoBehaviour
     float idleTimer = 0f;
     float idleDuration = 5f; // Time to stay idle (seconds)
     float walkToIdleTimer = 0f;
-    float walkToIdleDuration = 20f;
+    float walkToIdleDuration = 30f;
     float walkRandomTimer = 0f;
     float walkRandomDuration = 10f; // Time to walk randomly (seconds)
     float stopDistance = 0.8f;
@@ -31,6 +39,8 @@ public class Giraffe : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animation>();
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
@@ -47,15 +57,6 @@ public class Giraffe : MonoBehaviour
         switch (animalState)
         {
             case AnimalState.IDLE:
-                if (lookAt != null)
-                {
-                    RotateToTarget(lookAt);
-                }
-                if (moveTo != null)
-                {
-                    updateAnimalState(AnimalState.WALKING);
-                }
-
                 if (idleTimer >= idleDuration)
                 {
                     idleTimer = 0f;
@@ -63,28 +64,7 @@ public class Giraffe : MonoBehaviour
                 }
                 break;
             case AnimalState.WALKING:
-                if (lookAt != null)
-                {
-                    RotateToTarget(lookAt);
-                }
-                if (moveTo != null)
-                {
-                    float distanceToTarget = Vector3.Distance(transform.position, moveTo.position);
-                    if (distanceToTarget > stopDistance)
-                    {
-                        MoveToTarget(moveTo); // Call the MoveToTarget function
-                    }
-                }
-                else
-                {
-                    if (walkRandomTimer >= walkRandomDuration)
-                    {
-                        walkRandomTimer = 0f;
-                        Vector3 randomPosition = GetRandomWalkPosition();
-                        moveTo = new GameObject().transform; // Create a temporary transform for random walk
-                        moveTo.position = randomPosition;
-                    }
-                }
+                RandomWalk();
                 if (walkToIdleTimer >= walkToIdleDuration)
                 {
                     walkToIdleTimer = 0f;
@@ -101,11 +81,27 @@ public class Giraffe : MonoBehaviour
         }
     }
 
-    Vector3 GetRandomWalkPosition()
-    {
-        // Generate a random position within the walk radius
-        Vector3 randomOffset = Random.insideUnitSphere * randomWalkRadius;
-        return transform.position + randomOffset;
+    void RandomWalk(){
+        if(!walkPointSet){
+            SearchForDestination();
+        }
+        if(walkPointSet){
+            agent.SetDestination(destinationPoint);
+        }
+        if(Vector3.Distance(transform.position, destinationPoint) < 10){
+            walkPointSet = false;
+        }
+    }
+
+    void SearchForDestination(){
+        float z = Random.Range(-range, range);
+        float x = Random.Range(-range, range);
+
+        destinationPoint = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+
+        if(Physics.Raycast(destinationPoint, Vector3.down, groundLayer)){
+            walkPointSet = true;
+        }
     }
 
 
@@ -128,10 +124,10 @@ public class Giraffe : MonoBehaviour
     {
         this.animalState = animalState;
         if(animalState == AnimalState.WALKING){
-            anim.Play();
+            anim.Play("animation_walk");
         }
         else if(animalState == AnimalState.IDLE){
-            anim.Stop();
+            anim.Stop("animation_walk");
         }
     }
 
---
+++ Assets/Scripts/OffmapBoundary.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffmapBoundary : MonoBehaviour
+{
+    [SerializeField] GameObject preventOffmap;
+    [SerializeField] float boundaryMaxX;
+    [SerializeField] float boundaryMinX;
+    [SerializeField] float boundaryMaxY;
+    [SerializeField] float boundaryMinY;
+    [SerializeField] float boundaryMaxZ;
+    [SerializeField] float boundaryMinZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (
+            preventOffmap.transform.position.x > boundaryMaxX
+            || preventOffmap.transform.position.x < boundaryMinY
+            || preventOffmap.transform.position.y > boundaryMaxY
+            || preventOffmap.transform.position.y < boundaryMinY
+            || preventOffmap.transform.position.z > boundaryMaxZ
+            || preventOffmap.transform.position.z < boundaryMinZ
+            )
+        {
+            preventOffmap.transform.position = new Vector3(0, 0, 1);
+        }
+    }
+}
