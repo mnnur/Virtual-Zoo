1f5a0edd788a4ab0c20b47bc1615b657d53fa613
mmnur <nurramadan35@gmail.com>
2024-06-09 14:49:09 +0700

    audio

--- Assets/Scripts/FreezeCameraRotation.cs
+++ Assets/Scripts/FreezeCameraRotation.cs
@@ -1,28 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FreezeCameraRotation : MonoBehaviour
 {
-    private Transform currentTransform;
-    public bool freeze = false;
+    // Start is called before the first frame update
+    private Quaternion frozenRotation;
+    private GameObject mainCamera;
 
-    void Start()
+    public void freeze()
     {
-        currentTransform = transform;
+        // Find the main camera GameObject
+        mainCamera = GameObject.Find("Main Camera");
+
+        if (mainCamera != null)
+        {
+            Debug.Log("Main camera found!");
+
+            // Initialize the frozen rotation to the current rotation of the camera
+            frozenRotation = mainCamera.transform.rotation;
+        }
+        else
+        {
+            Debug.LogError("Main camera not found!");
+        }
     }
 
     void Update()
     {
-        if(freeze){
-            transform.position = currentTransform.position;
-            transform.rotation = currentTransform.rotation;
-        }else {
-            currentTransform = transform;
+        if (mainCamera != null)
+        {
+            // Set the rotation of the camera to the frozen rotation every frame
+            mainCamera.transform.rotation = frozenRotation;
         }
     }
 
-    public void SetFreeze(bool freeze){
-        this.freeze = freeze;
-    }
+    public void UnfreezeRotation()
+    {
+        if (mainCamera != null)
+        {
+            // Restore the original rotation of the camera
+            mainCamera.transform.rotation = frozenRotation;
+        }
+    }
 }
