151dc6943f88fdbd840bb90105667b2e3394301a
mmnur <nurramadan35@gmail.com>
2024-06-12 19:47:35 +0700

    editted some environment

--- Assets/Scripts/Bear.cs
+++ Assets/Scripts/Bear.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bear : MonoBehaviour
+public class Bear : Animal
 {
     // Start is called before the first frame update
-    void Start()
+    protected override void updateAnimalState(AnimalState animalState)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        this.animalState = animalState;
+        if(animalState == AnimalState.WALKING){
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
+            Debug.Log("Tiger walk");
+        }
+        else if(animalState == AnimalState.IDLE){
+            Debug.Log("Tiger idle");
+        }
+        else if(animalState == AnimalState.RUNNING){
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isWalking", false);
+        }
     }
 }
--- Assets/Scripts/Horse.cs
+++ Assets/Scripts/Horse.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Horse : MonoBehaviour
+public class Horse : Animal
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void updateAnimalState(AnimalState animalState)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        this.animalState = animalState;
+        if(animalState == AnimalState.WALKING){
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
+            Debug.Log("Tiger walk");
+        }
+        else if(animalState == AnimalState.IDLE){
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+            Debug.Log("Tiger idle");
+        }
+        else if(animalState == AnimalState.RUNNING){
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isWalking", false);
+        }
     }
 }
