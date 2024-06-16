022be091f8bde480e96281a590e1b1d3896a3830
mmnur <nurramadan35@gmail.com>
2024-06-16 15:13:39 +0700

    audio

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -6,11 +6,13 @@ public class Chest : Interactable
 {
     private bool isOpen = false;
     private SkinnedMeshRenderer skinnedMeshRenderer;
+    private AudioSource audioSource;
     GameObject chestUI;
     [SerializeField] Camera renderUICamera;
 
     void Awake(){
         skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        audioSource = GetComponent<AudioSource>();
         chestUI = transform.GetChild(0).gameObject;
         Canvas chestCanvas = chestUI.GetComponent<Canvas>();
         chestCanvas.worldCamera = renderUICamera;
@@ -22,9 +24,11 @@ public class Chest : Interactable
         if(isOpen){
             skinnedMeshRenderer.SetBlendShapeWeight(0, 0f);
             isOpen = false;
+            audioSource.Play();
         }else{
             skinnedMeshRenderer.SetBlendShapeWeight(0, 100f);
             isOpen = true;
+            audioSource.Play();
         }
     }
 }
